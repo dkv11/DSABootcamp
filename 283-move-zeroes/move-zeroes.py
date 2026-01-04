30dd5da0244a0a4e3dfe0c7a7a70d2f9622e3884
dkv11 <143937627+dkv11@users.noreply.github.com>
2026-01-04 19:53:08 +0530

    Time: 5 ms (50.66%) | Memory: 18.5 MB (94.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,11 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        while i < len(nums):
-            if nums[i] == 0:
-                nums.remove(nums[i])
-                nums.append(0)
-            i+=1
-            
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index] , nums[i] = nums[i], nums[index]
+
+                index +=1
         
